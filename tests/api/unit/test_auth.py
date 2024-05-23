d9549ac0cfe9f6db2f6c278dfd9142b89c790d4c
Ethan Knox <ethan@staticdata.io>
2024-05-23 18:54:04 -0400

    expired fails

--- tests/api/unit/test_auth.py
+++ tests/api/unit/test_auth.py
@@ -1,6 +1,7 @@
 import pytest
 from uuid import UUID
 from datetime import datetime, timezone, timedelta
+from jwt.exceptions import ExpiredSignatureError
 
 from app.controllers.auth import JWTTokenFlow
 from app.schemas.jtw_schema import JWTBase
@@ -38,3 +39,15 @@ class TestAuth:
         assert token.data["user"]["email_address"] == fixed_user.email_address
         assert token.data["user"]["id"] == token.data["sub"]
         assert not token.data["org"]
+
+    def test_auth_jwt_fails_expired(self, setup_state):
+        _, db_session  = setup_state
+        flow = JWTTokenFlow(db_session)
+        fixed_user = User(uid=UUID('11d52aeb-0dc3-4a90-a8b8-27b061c49920'), email="stubbed_email@email.test").create(db_session)
+        expired_token = ("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
+                         "eyJzdWIiOiJ1c2VyLTExZDUyYWViLTBkYzMtNG"
+                         "E5MC1hOGI4LTI3YjA2MWM0OTkyMCIsImV4cCI6"
+                         "MTcxNjQxODI3OH0.XadUn9x4rbYCuv7gVNGrSE"
+                         "fPZSnUH1gMZz6caCILdXw")
+        with pytest.raises(ExpiredSignatureError):
+            flow.get_auth_token(JWTBase(token=expired_token))
