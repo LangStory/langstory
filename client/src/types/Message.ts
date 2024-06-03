ef128b444bc44a07a11144e583be94ad0d5af5ef
Yoaquim Cintrón <yoaquim@outlook.com>
2024-06-02 20:25:31 -0400

    You can now see messages

--- client/src/types/Message.ts
+++ client/src/types/Message.ts
@@ -1,8 +1,10 @@
-export type MessageType = 'user' | 'message' | 'assistant' | 'tool'
+export type MessageRole = 'user' | 'system' | 'assistant' | 'tool'
+export type MessageType = 'system_message' | 'user_message' | 'assistant_message' | 'tool_message' | 'external_event'
 
 export default interface Message {
     id: string
-    role: MessageType
+    role: MessageRole
+    type: MessageType
     content: string
     chat_id: string
 }
